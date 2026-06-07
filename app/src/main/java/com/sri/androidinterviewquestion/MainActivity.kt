39ac17e3b7833385307beff09059650d81854645
Srivathsan <srivathsanbe@gmail.com>
2026-06-07 06:08:49 +0530

    Initial Commit

---
+++ app/src/main/java/com/sri/androidinterviewquestion/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.sri.androidinterviewquestion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sri.androidinterviewquestion.ui.theme.AndroidInterviewQuestionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidInterviewQuestionTheme {
+                Questions()
+            }
+        }
+    }
+}
+
+@Composable
+fun Questions() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidInterviewQuestionTheme {
+        Questions()
+    }
+}
